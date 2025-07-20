a008189dfe9fcc0a51a7278dfbca206c81b7b22b
KOTIREDDY63 <2100031641cser@gmail.com>
2025-07-21 00:04:58 +0530

    Add Mahindra Scorpio specifications to new Java class

---
+++ car/mahindrascorpio.java
@@ -0,0 +1,48 @@
+package car;
+
+public class mahindrascorpio {
+    public static void main(String[] args) {
+        
+   
+
+    String EngineType = "2.2L mHawk Diesel Engine";
+    String Displacement = "2179 cc";
+    String Maxtorque = "300 Nm @ 1800-2800 rpm";
+    int numberofcylinders = 4;
+    String fuelsupplysystem = "CRDI";
+    boolean turbocharged= true;
+    String fueltype = "Diesel";
+    String transmission = "6-Speed Manual Transmission";
+    String bootspace = "460 Liters";
+    String bodytype = "SUV";
+    String fuelcapacity = "60 Liters";
+
+    System.out.println("Maxtorque:" + Maxtorque);
+    System.out.println("Engine Type: " + EngineType);
+    System.out.println("Displacement: " + Displacement);       
+    System.out.println("Number of Cylinders: " + numberofcylinders);
+    System.out.println("Fuel Supply System: " + fuelsupplysystem);
+    System.out.println("Turbocharged: " + turbocharged);
+    System.out.println("Fuel Type: " + fueltype);
+    System.out.println("Transmission: " + transmission);
+        
+    System.out.println("Boot Space: " + bootspace);
+    System.out.println("Body Type: " + bodytype);
+    System.out.println("Fuel Capacity: " + fuelcapacity);   
+    System.out.println("Mahindra Scorpio Specifications:");
+    System.out.println("Engine Type: " + EngineType);
+    System.out.println("Displacement: " + Displacement);
+    System.out.println("Max Torque: " + Maxtorque);
+    System.out.println("Number of Cylinders: " + numberofcylinders);
+    System.out.println("Fuel Supply System: " + fuelsupplysystem);
+    System.out.println("Turbocharged: " + turbocharged);
+    System.out.println("Fuel Type: " + fueltype);
+    System.out.println("Transmission: " + transmission);
+    System.out.println("Boot Space: " + bootspace);
+    System.out.println("Body Type: " + bodytype);
+    System.out.println("Fuel Capacity: " + fuelcapacity);
+   
+
+ }
+}
+
