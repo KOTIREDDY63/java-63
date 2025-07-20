0b309f789d3e2a97fe6d7dfcde0008329b1ed789
KOTIREDDY63 <2100031641cser@gmail.com>
2025-07-20 23:39:00 +0530

    studentdata

---
+++ Second_programme/LMS_VARIABLES.java
@@ -0,0 +1,19 @@
+package Second_programme;
+
+public class LMS_VARIABLES {
+    public static void main(String[] args) {
+        
+    
+short studentID=101;
+String studentName="John Doe";
+String studentEmail="john@gmail.com";
+String studentPassword="PuhuiB@i79";
+char studentGender='M';
+boolean iSstudentMale=true;
+long studentPhoneNumber=1234567890;
+
+System.out.println("studentID:"+studentID);
+System.out.println("studentName:"+studentName + studentEmail);
+System.out.println("studentgenderMale:"+ iSstudentMale);
+}
+}
---
+++ Second_programme/variables.java
@@ -0,0 +1,19 @@
+package Second_programme;
+public class variables {
+   static int Somedata;
+    String Brand = "HIGHFLIGER";
+    String Description = "This is a high-quality brand known for its stylish clothing.";
+    float rating = 5.0f;
+    int price = 100;
+
+    public static void main(String[] args) {
+       
+int somedataNew1 = 10;
+  int somedataNew2;
+       
+System.out.println(somedataNew1);
+
+        System.out.println(Somedata);
+    }
+
+}
