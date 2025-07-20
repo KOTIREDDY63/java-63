68a7c03065a26c2c659e8ce7a8e0be076f048f8e
KOTIREDDY63 <2100031641cser@gmail.com>
2025-07-20 22:46:32 +0530

    hello_world

---
+++ First_programme/app.java
@@ -0,0 +1,11 @@
+package First_programme;
+
+public class app {
+public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    System.out.println("This is my first Java program.");
+    System.out.println("I am learning how to write Java code.");
+    System.out.println("Java is a versatile programming language.");
+    System.out.println("Let's keep coding and improving our skills!");
+}
+}
