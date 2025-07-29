aba6304c3d1d1e394021467cbb35dc502a9f326f
KOTIREDDY63 <2100031641cser@gmail.com>
2025-07-29 15:58:15 +0530

    increment_decrement_operators

---
+++ operators/increment_decrement.java
@@ -0,0 +1,21 @@
+package operators;
+
+public class increment_decrement {
+    public static void main(String[] args) {
+        
+   
+     int a = 10;
+
+     System.out.println("A = " + a);
+      System.out.println("++A: " + ++a);//pre increment
+      System.out.println("A++ :"  + a++ );//post increment
+       System.out.println("A = " + a);
+        
+        System.out.println("++A: " + --a);//pre decrement
+      System.out.println("A++ :"  + a-- );//post decrement
+       System.out.println("A = " + a);
+        
+    
+    }
+
+}
---
+++ operators/relational_operatiors.java
@@ -0,0 +1,21 @@
+package operators;
+
+public class relational_operatiors {
+
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+
+        System.out.println("A = " + a);
+        System.out.println("B = " + b);
+
+        System.out.println("A == B: " + (a == b)); // equal to
+        System.out.println("A != B: " + (a != b)); // not equal to
+        System.out.println("A > B: " + (a > b));   // greater than
+        System.out.println("A < B: " + (a < b));   // less than
+        System.out.println("A >= B: " + (a >= b)); // greater than or equal to
+        System.out.println("A <= B: " + (a <= b)); // less than or equal to
+
+
+        
+    }
+}
