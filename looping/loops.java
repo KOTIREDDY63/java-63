157eadaf1368fab9837907f4f4e23ccd60b9d84f
KOTIREDDY63 <2100031641cser@gmail.com>
2025-07-31 09:48:11 +0530

    for loop and while loops

---
+++ looping/loops.java
@@ -0,0 +1,27 @@
+package looping;
+
+public class loops {
+    public static void main(String[] args) {
+        //for loop
+        // This loop will run 5 times, from 0 to 4
+        for (int i = 5; i >= 1; i--) {
+            if(i==3){
+                break; // This will exit the loop when i is 3
+            }
+            System.out.println(+i) ;
+        }
+        //continue statement example
+        for (int i = 0; i <= 5; i++) {
+            if(i==3){
+                continue; // This will skip the current iteration when i is 3
+            }
+            System.out.println(+i) ;
+        }
+        //while loop
+        // This loop will run as long as the condition is true
+        // while (true) {
+        //     System.out.println(8);   
+        // }
+        
+    }
+}
